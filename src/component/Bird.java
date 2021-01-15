5fd0173c421722693f44835d251fb80856b50c6c
OrangeFlavoredDerek <derekchan0407@gmail.com>
2021-01-15 22:55:11 +0800

    # update

---
+++ src/component/Bird.java
@@ -0,0 +1,49 @@
+package component;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
+
+import util.Constant;
+import util.GameUtil;
+
+//小鸟类,实现小鸟的绘制与飞行逻辑
+
+public class Bird {
+    public static final int imageCount = 8;//图片数量
+    public static final int stateCount = 4;//状态数
+    private final BufferedImage[][] birdImages;//小鸟的图片数组对象
+    private int x;
+    private int y;//小鸟的坐标
+    private int wingState;//翅膀状态
+
+    //图片资源
+    private BufferedImage image;
+
+    //小鸟的状态
+    private int state;
+    public static final int birdNormal = 0;
+    public static final int birdUp = 1;
+    public static final int birdFall = 2;
+    public static final int birdDeadFall = 3;
+    public static final int birdDead = 4;
+    private final Rectangle birdCollisionRect;//碰撞矩形
+    public static final int rectDescale = 2; // 补偿碰撞矩形宽高的参数
+
+    private final ScoreCounter counter; // 计分器
+    private final GameOverAnimation gameOverAnimation;
+
+    public static int birdWidth;
+    public static int birdHeight;
+
+    //在构造器中对资源初始化
+    public Bird() {
+        counter = ScoreCounter.getInstance();
+    }
+
+    public boolean isDead() {
+        return true;
+    }
+
+}
---
+++ src/component/ScoreCounter.java
@@ -0,0 +1,74 @@
+package component;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import util.Constant;
+
+/**
+ * 游戏计时器，使用静态内部类实现了单例模式
+ * 
+ */
+
+public class ScoreCounter {
+    private static class ScoreCounterHolder {
+        private static final ScoreCounter scoreCounter = new ScoreCounter();
+    }
+
+    public static ScoreCounter getInstance() {
+        return ScoreCounterHolder.scoreCounter;
+    }
+
+    private long score = 0;//分数
+    private long bestScore;//最高分
+
+    private ScoreCounter() {
+        bestScore = -1;
+        try {
+            loadBestScore();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //装载最高纪录
+    /**
+     * 此处用到了对文件操作
+     * @throws Exception
+     */
+    private void loadBestScore() throws Exception {
+        File file = new File(Constant.scoreFilePath);
+        if (file.exists()) {
+            DataInputStream dis = new DataInputStream(new FileInputStream(file));
+            bestScore = dis.readLong();
+            dis.close();
+        }
+    }
+
+    //保存分数
+    public void saveScore() {
+        bestScore = Math.max(bestScore, getCurrentScore());
+        try {
+            File file = new File(Constant.scoreFilePath);
+            DataOutputStream dos = new DataOutputStream(new FileOutputStream(file));
+            dos.writeLong(bestScore);//写入文件
+            dos.close();//关闭文件
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void score(Bird bird) {
+        score += 1;
+    }
+
+    public long getBestScore() {
+        return bestScore;
+    }
+
+    public long getCurrentScore() {
+        return score;
+    }
+}
